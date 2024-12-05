1bf55489649179a048074e8f5b2506af37ba169d
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-04 19:04:18 -0800

    fixed on css thank you glenn

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -5,11 +5,16 @@ const Ingredient = ({name, index, parentName='IngredientList', handleFunction })
     <div style={{
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
     }}>
 
+        <div>
         {name}
+        </div>
 
-        {
+       <div>
+       {
             parentName === 'IngredientList' ?
             (
                 <button onClick={() => handleFunction(index)}>+</button>
@@ -17,6 +22,7 @@ const Ingredient = ({name, index, parentName='IngredientList', handleFunction })
                 <button onClick={() => handleFunction(index)}>-</button>
             )
         }
+       </div>
 
     </div>
   )
